89bb5da72a9f3b9b262fb8026f2ebfcd85c26749
rohitbhandari007 <rohitbhandari20561210@gmail.com>
2023-06-25 19:18:04 +0545

    feat: bit ready to push

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let gameArea = document.querySelector(".game-area");
 let gameOverDiv = document.querySelector(".game-over");
 let scoreDiv = document.querySelector(".score-board");
 let gridSize = 150;
-console.log(gameArea);
 
 function generateRandomArray(ArrayLength) {
   const randomArray = [];
@@ -41,7 +40,7 @@ function genrateMinesArray(ArrayLength) {
   return randomArray;
 }
 
-let minesArray = genrateMinesArray(10);
+let minesArray = genrateMinesArray(20);
 let path = [];
 let gameOverBool = false;
 let gameStatus = "running";
@@ -57,21 +56,58 @@ const gameOver = () => {
   gameStatus = "stop";
   console.log(gameStatus, "--inside game over function");
 };
+const calculateMines = (cellId) => {
+  let cell = document.getElementById(cellId);
 
-const selectPath = (status, cell) => {
+  let totalThreats = 0;
+  let n = cellId;
+
+  let leftSide = parseInt(n) - 1;
+  let rightSide = parseInt(n) + 1;
+  let topLeft = parseInt(n) - 16;
+  let topMiddle = parseInt(n) - 15;
+  let topRight = parseInt(n) - 14;
+  let bottomLeft = parseInt(n) + 14;
+  let bottomMiddle = parseInt(n) + 15;
+  let bottomRight = parseInt(n) + 16;
+
+  let threatsAroundCell = [
+    leftSide,
+    rightSide,
+    topLeft,
+    topMiddle,
+    topRight,
+    bottomLeft,
+    bottomMiddle,
+    bottomRight,
+  ];
+  console.log(threatsAroundCell);
+  minesArray.forEach((item) => {
+    if (threatsAroundCell.includes(item)) {
+      totalThreats++;
+      cell.innerHTML = totalThreats;
+    } else {
+      cell.innerHTML = totalThreats;
+    }
+  });
+};
+const selectPath = (cell) => {
   cell.addEventListener("click", () => {
-    console.log(gameStatus);
     if (
       blockedPathArray.includes(parseInt(cell.id)) === false &&
       minesArray.includes(parseInt(cell.id)) === false &&
       gameStatus === "running"
     ) {
-      console.log(cell.id);
       cell.style.background = "rgb(70, 157, 85)";
       cell.style.borderColor = "rgb(70, 157, 85)";
-      path.push(cell.id);
-      score++;
-      scoreDiv.innerHTML = "Score: " + score;
+      if (path.includes(cell.id)) {
+        console.log("hi");
+      } else {
+        calculateMines(cell.id);
+        path.push(cell.id);
+        score++;
+        scoreDiv.innerHTML = "Score: " + score;
+      }
     }
     if (
       minesArray.includes(parseInt(cell.id)) === true &&
@@ -87,7 +123,7 @@ const createBlockedPath = (cellArray) => {
   cellArray.forEach((item) => {
     let cell = document.getElementById(item);
 
-    cell.style.background = "gray";
+    cell.style.background = "#333";
     cell.style.cursor = "default";
   });
 };
@@ -96,7 +132,7 @@ const createMinesc = (cellArray) => {
   cellArray.forEach((item) => {
     let cell = document.getElementById(item);
 
-    cell.style.background = "none";
+    // cell.style.background = "red";
   });
 };
 const createGrid = () => {
@@ -104,13 +140,26 @@ const createGrid = () => {
     let cell = document.createElement("div");
     cell.className = "cell";
     cell.id = i;
+    // cell.innerHTML = cell.id;
     console.log(gameStatus);
 
-    selectPath(gameStatus, cell);
+    selectPath(cell);
 
     gameArea.appendChild(cell);
   }
 };
-createGrid();
-createBlockedPath(blockedPathArray);
-createMinesc(minesArray);
+
+const startGame = () => {
+  createGrid();
+  createBlockedPath(blockedPathArray);
+  createMinesc(minesArray);
+  document.querySelector(".start-game-btn").style.display = "none";
+  document.querySelector(".restart-game-btn").style.display = "flex";
+
+  document.querySelector(".score-board").style.opacity = "1";
+};
+
+const restartGame = () => {
+  window.parent.location = window.parent.location.href;
+  // startGame();
+};
